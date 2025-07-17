dbe8115da05e0a771049bace4f6bf9e9d2a9e69c
fawkesdbs <s.mazibuko1108@gmail.com>
2025-07-17 19:10:15 +0200

    Update CORS

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ const taskRoutes = require("./routes/Tasks");
 const app = express();
 app.use(
   cors({
-    // origin: true,
-    origin: ["http://localhost:3000", "https://internal.digital-mafia.co.za/"],
+    origin: true,
+    // origin: ["http://localhost:3000", "https://internal.digital-mafia.co.za/"],
   })
 );
 app.use(express.json());
